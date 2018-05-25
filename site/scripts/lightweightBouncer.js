3c9a982f9c99f71ffeb5349392941f251e90f4d1
Deacon Rodda <deacon@sqglz.com>
2018-05-24 18:51:47 -0600

    Lightweight Bouncer now case insensitive.

---
+++ site/scripts/lightweightBouncer.js
@@ -0,0 +1,26 @@
+
+
+
+
+var whiteList = ["melissa martinez","deacon rodda","2chefmelissa@gmail.com","1uppity1@gmail.com","betsy lamberson","mel", "kelley kavanaugh", "sara king"];
+var $bouncer = $("#bouncer");
+var $nameSubmission;
+var $bouncerGoverned = $("#bouncer-governed");
+var $bouncerForm = $(".bouncer__form-box");
+
+$bouncer.on("submit", function(e) {
+	e.preventDefault();
+	nameSubmission = $("#bouncer").find('[type=text]').val();
+	// nameSubmission = $nameSubmission.val
+	console.log(nameSubmission);
+	var attempt = nameSubmission.toLowerCase();
+	console.log(attempt);
+	for (var i = 0; i < whiteList.length; i++) {
+		if(attempt===whiteList[i]) {
+			// attempt = $("#bouncer").find('[type=text]').val("yep.");
+			$bouncerGoverned.removeClass("bouncer-governed--blurred");
+			$bouncerForm.addClass("bouncer__form-box--hidden");
+			return;
+		} 
+	}
+});

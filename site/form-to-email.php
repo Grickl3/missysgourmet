3f8d94d52ec4e762767bf98a3ac2db6c08b6de20
DeaconRodda <deacon@sqglz.com>
2018-07-17 15:22:37 -0600

    the last test worked, so I'm swapping in Mel's address and dealing with the concatenation glitch until switching over to a node based solution.

--- site/form-to-email.php
+++ site/form-to-email.php
@@ -21,12 +21,12 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'deacon@sqglz.com';//<== update the email address
+$email_from = '2chefmelissa@gmail.com';//<== update the email address
 $email_subject = "New Form Submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "deacon@sqglz.com";//<== update the email address
+$to = "2chefmelissa@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!

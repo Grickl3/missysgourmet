c6815401e41f5adfd42e0739657b391b2de0bbf6
DeaconRodda <deacon@sqglz.com>
2018-07-17 15:17:53 -0600

    that broke everything so I removed the semicolon.

--- site/form-to-email.php
+++ site/form-to-email.php
@@ -24,7 +24,7 @@ if(IsInjected($visitor_email))
 $email_from = 'deacon@sqglz.com';//<== update the email address
 $email_subject = "New Form Submission";
 $email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".;
+    "Here is the message:\n $message".
     
 $to = "deacon@sqglz.com";//<== update the email address
 $headers = "From: $email_from \r\n";
